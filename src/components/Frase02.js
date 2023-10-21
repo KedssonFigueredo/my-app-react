2b1282cb4fa26e14553bf3df78fe1e06d931d866
Kedsson Figueredo <kedsson19@gmail.com>
2023-10-21 17:09:41 -0200

    all changes

---
+++ src/components/Frase02.js
@@ -0,0 +1,11 @@
+function Frase02() {
+    return (
+
+        <div>
+            <h1>O</h1>
+        </div>
+    )
+
+}
+
+export default Frase02;
---
+++ src/components/Frases.js
@@ -0,0 +1,17 @@
+import Frase01 from "./Frase01";
+import Frase02 from "./Frase02";
+import Frase03 from "./Frase03";
+
+function Frases() {
+    return (
+        <div>
+            <h1>Frase que eu  mais digo no meu dia a dia</h1>
+            <hr></hr>
+            <Frase01 />
+            <Frase02 />
+            <Frase03 />
+        </div>
+    )
+}
+
+export default Frases;
